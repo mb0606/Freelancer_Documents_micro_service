063c4a92a879da6f952df815a1dcdf7f8fae4bf6
Marco Berardini <mberardini@evolution-man.com>
2017-04-16 16:26:19 -0700

    [server] Created seed data for document.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+  # create_table "freelance_documents", force: :cascade do |t|
+  #   t.string   "title"
+  #   t.string   "description"
+  #   t.text     "file_url"
+  #   t.text     "image_url"
+  #   t.datetime "created_at",  null: false
+  #   t.datetime "updated_at",  null: false
+  # end
+10.times do |d|
+  FreelanceDocument.create!(title:"Doc #{d}", description:"Pariatur aliqua proident excepteur pariatur sint velit velit et ut non sunt aliquip pariatur.Pariatur in anim in esse minim anim consectetur fugiat ut enim ullamco.", file_url: 'https://netorgft860983-my.sharepoint.com/personal/mberardini_evolution-man_com/_layouts/15/guestaccess.aspx?docid=1b9e2fdd2beae48e884c780879408a517&authkey=AZldI--JL6GWQOwKAwn_FoU', image_url: 'https://www.project-hub.net/img/hero.png')
+end
